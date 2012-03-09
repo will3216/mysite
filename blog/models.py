8b2604e66015008ccc569d010e4c46e648451fdc
William Bryant <brawrg1@hotmail.com>
2012-03-09 17:10:56 -0600

    first commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+class Post(models.Model):
+
+	title = models.CharField(max_length=60)
+	body = models.TextField()
+	created = models.DateTimeField()
+	tags = TaggableManager()
+	#posted_by = user
+	
+	
+	
+	def __unicode__(self):
+		return self.title
